eaf507737ade40288903a149b5023af37f059889
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-04-30 17:14:28 -0400

    added styling

--- bike-ui/src/app/components/view-registration/view-registration.component.ts
+++ bike-ui/src/app/components/view-registration/view-registration.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BikeService } from "../../services/bike.service";
+import { ActivatedRoute } from '@angular/router'; 
 
 @Component({
-  selector: 'app-view-registration',
-  templateUrl: './view-registration.component.html',
-  styleUrls: ['./view-registration.component.css']
+  selector: "app-view-registration",
+  templateUrl: "./view-registration.component.html",
+  styleUrls: ["./view-registration.component.css"],
 })
 export class ViewRegistrationComponent implements OnInit {
+  public bikeReg;
 
-  constructor() { }
+  constructor(private bikeService: BikeService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.getBikeReg(this.route.snapshot.params.id); 
   }
 
+  getBikeReg(id: number) {
+    this.bikeService.getBike(id).subscribe(
+      (data) => {
+        this.bikeReg = data;
+      },
+      (err) => console.log(err),
+      () => console.log("bike loaded")
+    );
+  }
 }
