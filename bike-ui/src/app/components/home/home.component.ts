118f5b2e984d162afe45e033f2f72b90cd48a829
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-04-30 16:35:49 -0400

    added view-registration component

--- bike-ui/src/app/components/home/home.component.ts
+++ bike-ui/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { BikeService } from "../../services/bike.service";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Observable } from 'rxjs'; 
 
 @Component({
   selector: "app-home",
@@ -16,14 +17,32 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.bikeform = new FormGroup({
-      name: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
-      phone: new FormControl('', Validators.required),
-      model: new FormControl('', Validators.required),
-      serialNumber: new FormControl('', Validators.required),
-      purchasePrice: new FormControl('', Validators.required),
-      purchaseDate: new FormControl('', Validators.required),
+      name: new FormControl("", Validators.required),
+      email: new FormControl("", Validators.required),
+      phone: new FormControl("", Validators.required),
+      model: new FormControl("", Validators.required),
+      serialNumber: new FormControl("", Validators.required),
+      purchasePrice: new FormControl("", Validators.required),
+      purchaseDate: new FormControl("", Validators.required),
       contact: new FormControl(),
-    })
+    });
+  }
+
+  submitRegistration() {
+    if (this.bikeform.valid) {
+      this.validMessage =
+        "Your bike registration has been submitted. Thank you!";
+      this.bikeService.createBikeRegistration(this.bikeform.value).subscribe(
+        (data) => {
+          this.bikeform.reset();
+          return true;
+        },
+        (error) => {
+          return Observable.throw(error);
+        }
+      );
+    } else {
+      this.validMessage = "Please fill out the form before submitting!";
+    }
   }
 }
